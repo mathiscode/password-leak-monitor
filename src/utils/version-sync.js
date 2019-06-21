9793f7539d78ae1395e8e443ea32123c983dba0a
J.R. Mathis <mathiscode@users.noreply.github.com>
2019-06-21 13:53:18 -0500

    synchronized versioning

--- src/utils/version-sync.js
+++ src/utils/version-sync.js
@@ -1,9 +1,11 @@
 const fs = require('fs')
-const Package = require('./package.json')
-const Manifest = require('./extension/manifest.json')
+const path = require('path')
+
+const Package = require(path.resolve(process.cwd(), 'package.json'))
+const Manifest = require(path.resolve(process.cwd(), 'extension/manifest.json'))
 
 if (Package.version !== Manifest.version) {
   Manifest.version = Package.version
-  fs.writeFileSync('./extension/manifest.json', JSON.stringify(Manifest, null, 2))
+  fs.writeFileSync(path.resolve(process.cwd(), 'extension/manifest.json'), JSON.stringify(Manifest, null, 2))
   console.log(`Updated manifest to ${Manifest.version}`)
 }
